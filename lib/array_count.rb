76894793a5db66e63005dc7eb1ca3f5324470764
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-16 05:08:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
   array.count do |element|
-    element == string
+    element.any? string
   
   # Return the total number of strings in the provided array using the count enumerable
 end
